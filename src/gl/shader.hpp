32f70b466e966c15c0146ddbf7258b237bc1d4eb
Joshua Reibert <joshua.reibert@uni-weimar.de>
2020-12-03 10:06:52 +0100

    Add OpenGL helper code

---
+++ src/gl/shader.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <fstream>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+
+#include <glad/glad.h>
+#include <spdlog/spdlog.h>
+
+namespace vizcraft {
+
+namespace gl {
+
+GLenum detect_shader_type_from_filename(const std::string& filename);
+std::string load_shader_source_from_file(const std::string& filename);
+bool compile_shader(GLuint shader);
+GLuint load_shader_from_file(const std::string& filename, GLenum type = GL_NONE);
+
+}  // namespace gl
+
+}  // namespace vizcraft
